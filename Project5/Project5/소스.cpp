e25df93fece05cf9594a30cf9a546dffb0d86a54
KIMSANGHWAN <KIMSANGHWAN@DESKTOP-8EB5LKO>
2020-05-15 14:44:28 +0900

    Student project

---
+++ Project5/Project5/소스.cpp
@@ -0,0 +1,19 @@
+//생성자 중복 정의
+//Program.cpp
+#include "Student.h"
+int main()
+{
+	Student *stu1 = new Student();
+	Student *stu2 = new Student(3);
+	Student *stu3 = new Student(3, "홍길동");
+
+	stu1->View();
+	stu2->View();
+	stu3->View();
+
+	delete stu1;
+	delete stu2;
+	delete stu3;
+
+	return 0;
+}
